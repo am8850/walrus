88702ea5d4b9840a3e7db1e77958112ab32f2893
Alex Morales <am8850mia@gmail.com>
2024-11-19 22:08:06 -0500

    Cleanup

--- src/DPSIW.Common/Models/Models.cs
+++ src/DPSIW.Common/Models/Models.cs
@@ -1,10 +1,4 @@
-﻿using System.Text.Json;
-
-namespace DPSIW.Common.Models;
-
-//public record Message(string type, string pid, string cid, JsonElement metadata);
-//public record Metadata(string file_url, string blob_id);
-//public record MedicalNoteAgent(string type, string pid, string cid, Metadata metadata);
+﻿namespace DPSIW.Common.Models;
 
 public record MedicalNotesMetadata(
     string type = "url",
